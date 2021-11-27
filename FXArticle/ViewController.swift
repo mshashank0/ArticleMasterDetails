4120a3d3dee65253bb52d7157df26c602f9758c5
Shashank Mishra <shashankm@exzeo.com>
2021-11-27 10:38:46 +0530

    Initial code

---
+++ FXArticle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FXArticle
+//
+//  Created by Shashank Mishra on 27/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+an application that displays a master/detail screen with a list of FX related articles and presents the details of the selected article.
